742dfb0e6441b87b61c5d7b02e2bb67e352581ae
Navid <navidjalili3@gmail.com>
2023-08-12 13:24:06 +0330

    Write binary search algorithm in Csharp

---
+++ chapter01_introduction_to_algorithms/csharp/BinarySearchApp/Program.cs
@@ -0,0 +1,23 @@
+﻿List<int> myList = new List<int>() { 1, 2, 3, 4, 5, 6 };
+
+Console.WriteLine(BinarySearch(myList, 2));
+
+static int BinarySearch(List<int> list, int element)
+{
+    int low = 0;
+    int high = list.Count - 1;
+
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        int guess = list[mid];
+
+        if (guess == element)
+            return mid;
+        else if (guess > element)
+            high = mid - 1;
+        else
+            low = mid + 1;
+    }
+    return -1;
+}
